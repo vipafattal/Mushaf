a7a5b23767c0ef5d294dc06a2c98cb7febdc4696
Abed Fattal <vipafattal@gmail.com>
2020-03-04 13:53:45 +0200

    Text not visible in dark mode fixed

--- app/src/main/java/co/jp/smagroup/musahaf/utils/extensions/Spanndable.kt
+++ app/src/main/java/co/jp/smagroup/musahaf/utils/extensions/Spanndable.kt
@@ -3,12 +3,15 @@ package co.jp.smagroup.musahaf.utils.extensions
 import android.text.Spannable
 import co.jp.smagroup.musahaf.utils.ClickableImageSpan
 
-fun whiteSpaceMagnifier(text: String): String =
-    text.replace(" ", "  ")
+fun whiteSpaceMagnifier(text: String): String {
+    //text.replace(" ", " ")
+    return text
+}
 
 
 fun Spannable.isClickableSpan(start: Int, end: Int): Boolean {
-    val styleSpans: Array<out ClickableImageSpan> = getSpans(start, end, ClickableImageSpan::class.java)
+    val styleSpans: Array<out ClickableImageSpan> =
+        getSpans(start, end, ClickableImageSpan::class.java)
     var isClickableSpan = false
     for (style in styleSpans) {
         isClickableSpan = true
