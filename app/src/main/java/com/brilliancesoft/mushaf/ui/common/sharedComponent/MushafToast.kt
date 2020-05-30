ac758c189929ab218a284f3b3c29afe5cebdfbdd
Abed Fattal <vipafattal@gmail.com>
2020-05-30 08:55:27 +0300

    show Quran popup from outside throw CastException, fixing worn Basmalih removal from first aya, Search popup not showing when search

--- app/src/main/java/com/brilliancesoft/mushaf/ui/common/sharedComponent/MushafToast.kt
+++ app/src/main/java/com/brilliancesoft/mushaf/ui/common/sharedComponent/MushafToast.kt
@@ -15,66 +15,54 @@ object MushafToast {
     @Retention(AnnotationRetention.SOURCE)
     annotation class ToastDuration
 
-        @JvmStatic
-        fun make(context: Context, message: String, @ToastDuration showingDuration: Int) {
-            val layout = View.inflate(context, R.layout.layout_toast, null)
+    @JvmStatic
+    fun make(context: Context, message: String, @ToastDuration showingDuration: Int) {
+        Toast(context).apply {
+            view = View.inflate(context, R.layout.layout_toast, null)
+                .apply { toast_text.text = message }
+            duration = showingDuration
+        }.show()
+    }
 
-            layout.toast_text.text = message
-            val toast = Toast(context)
-            with(toast) {
-                view = layout
-                duration = showingDuration
-                show()
-            }
-        }
+    @JvmStatic
+    fun make(context: Context, @StringRes message: Int, @ToastDuration showingDuration: Int) {
+        make(context, context.getString(message), showingDuration)
+    }
 
-        @JvmStatic
-        fun make(context: Context, @StringRes message: Int, @ToastDuration showingDuration: Int) {
-            val layout = View.inflate(context, R.layout.layout_toast, null)
+    @JvmStatic
+    fun makeShort(context: Context, message: String) {
+        make(
+            context,
+            message,
+            Toast.LENGTH_SHORT
+        )
+    }
 
-            layout.toast_text.text = context.getString(message)
-            val toast = Toast(context)
-            with(toast) {
-                view = layout
-                duration = showingDuration
-                show()
-            }
-        }
+    @JvmStatic
+    fun makeLong(context: Context, message: String) {
+        make(
+            context,
+            message,
+            Toast.LENGTH_LONG
+        )
+    }
 
-        @JvmStatic
-        fun makeShort(context: Context, message: String) {
-            make(
-                context,
-                message,
-                Toast.LENGTH_SHORT
-            )
-        }
+    @JvmStatic
+    fun makeShort(context: Context, @StringRes message: Int) {
+        make(
+            context,
+            message,
+            Toast.LENGTH_SHORT
+        )
+    }
 
-        @JvmStatic
-        fun makeLong(context: Context, message: String) {
-            make(
-                context,
-                message,
-                Toast.LENGTH_LONG
-            )
-        }
-
-        @JvmStatic
-        fun makeShort(context: Context, @StringRes message: Int) {
-            make(
-                context,
-                message,
-                Toast.LENGTH_SHORT
-            )
-        }
-
-        @JvmStatic
-        fun makeLong(context: Context, @StringRes message: Int) {
-            make(
-                context,
-                message,
-                Toast.LENGTH_LONG
-            )
-        }
+    @JvmStatic
+    fun makeLong(context: Context, @StringRes message: Int) {
+        make(
+            context,
+            message,
+            Toast.LENGTH_LONG
+        )
+    }
 
 }
